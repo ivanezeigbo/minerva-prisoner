bc6d6b590ee1e3f74d37d2e404e54f1b84f49df7
Al Ng <alng@Als-MacBook-Pro.local>
2018-03-08 12:30:46 +0530

    change manage.py

--- manage.py
+++ manage.py
@@ -2,9 +2,14 @@
 import os
 import sys
 
-
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
-    from otree.management.cli import execute_from_command_line
-    execute_from_command_line(sys.argv, script_file=__file__)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+                          "Couldn't import Django. Are you sure it's installed and "
+                          "available on your PYTHONPATH environment variable? Did you "
+                          "forget to activate a virtual environment?"
+                          )
+execute_from_command_line(sys.argv)
